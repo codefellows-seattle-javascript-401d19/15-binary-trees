441cec50b15aa2dae3eb7f3d53b139b78f839166
Cameron Moorehead <hello@cameronmoorehead.com>
2017-12-15 15:52:11 -0800

    scaffolding / first commit

---
+++ lab-cameron/lib/binary-tree.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const BinaryTree = function(value) {
+  this.value = value;
+  this.left = left;
+  this.right = right;
+}
+
+BinaryTree.prototype.find = function(value) {
+  // do something
+}
