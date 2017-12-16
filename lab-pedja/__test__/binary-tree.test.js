cbe9a0c9f81228315085c0ade0ee669785c0dde2
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-16 14:55:52 -0800

    adding FIND method and passing tests for it

---
+++ lab-pedja/__test__/binary-tree.test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let BinaryTree = require('../lib/binary-tree');
+
+describe('binary-tree.js', () => {
+  test('FIND method should return node with a specified value', () => {
+    expect(BinaryTree.prototype.find(1)).toEqual(1);
+    expect(BinaryTree.prototype.find(2)).toEqual(2);
+    expect(BinaryTree.prototype.find(3)).toEqual(3);
+    expect(BinaryTree.prototype.find(4)).toEqual(4);
+  });
+  test.only('FIND method should return null if value is not found', () => {
+    expect(BinaryTree.prototype.find(6)).toEqual(undefined);
+
+  });
+  test('FIND method should throw error if value is not a number', () => {
+    expect(() => BinaryTree.prototype.find('testing')).toThrow();
+  });
+});
+
---
+++ lab-pedja/lib/binary-tree.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function BinaryTree (value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+BinaryTree.prototype.find = function (value) {
+  if(typeof value !== 'number'){
+    throw new TypeError('value should be a number');
+  }
+  let one = new BinaryTree(1);
+  let two = new BinaryTree(2);
+  let three = new BinaryTree(3);
+  let four = new BinaryTree(4);
+  let five = new BinaryTree(5);
+  
+  one.left = two;
+  one.right = three;
+  three.left = four;
+  three.right = five;
+
+  let foundNode;
+  let inOrderTraversal = function (root) {
+    if(root === null) {
+      return;
+    }
+
+    inOrderTraversal(root.left);
+    if(value === root.value) {
+      foundNode = value;
+    }
+    inOrderTraversal(root.right);
+  };
+  if(foundNode === undefined){
+    foundNode = null;
+  }
+  inOrderTraversal(one);
+  return foundNode;
+};
+
+module.exports = BinaryTree;
