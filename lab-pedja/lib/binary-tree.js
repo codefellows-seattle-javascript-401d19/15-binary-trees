2e03c07902539f3aa41a53ea5a3717e52aba81b4
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-16 15:43:29 -0800

    starting toString method

--- lab-pedja/lib/binary-tree.js
+++ lab-pedja/lib/binary-tree.js
@@ -40,4 +40,41 @@ BinaryTree.prototype.find = function (value) {
   return foundNode;
 };
 
+// TODO toString(str) should iterate over all child nodes using a pre-order traversal and concatenate their values separated by newlines in to a string
+
+BinaryTree.prototype.toString = function (value) {
+  if(typeof value !== 'number'){
+    throw new TypeError('value should be a number');
+  }
+  let one = new BinaryTree(1);
+  let two = new BinaryTree(2);
+  let three = new BinaryTree(3);
+  let four = new BinaryTree(4);
+  let five = new BinaryTree(5);
+  
+  one.left = two;
+  one.right = three;
+  three.left = four;
+  three.right = five;
+
+  // TODO add stringify method here
+  let foundNode;
+  let inOrderTraversal = function (root) {
+    if(root === null) {
+      return;
+    }
+
+    inOrderTraversal(root.left);
+    if(value === root.value) {
+      foundNode = value;
+    }
+    inOrderTraversal(root.right);
+  };
+  if(foundNode === undefined){
+    foundNode = null;
+  }
+  inOrderTraversal(one);
+  return foundNode;
+};
+
 module.exports = BinaryTree;
