ea9a4716c8d28c008ca1c7245c045b2d89b9cd90
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-16 13:15:52 -0800

    trying to refactor find method still

--- lab-shannon/lib/binaryTrees.js
+++ lab-shannon/lib/binaryTrees.js
@@ -6,27 +6,53 @@ function BinaryTree(value){
   this.right = null;
 
   this.find = function(value){  //in-order traversal (left-root-right)
-    // as long as there is a left element, go left
-    // when there are no elements to the left, see if the current value is what we want
-    // if it's not, see if there is a value to the right; does it have a left node?
-    // if it does go left as long as there are left nodes
-    // when there are no elements to the left, see if the current value is what we want
+    let leftElement = this.left;
+    let rightElement = this.right;
 
-    if(this.left)
-      this.left.find(value);
-
-    if(this.value === value)
-      return this.value;
-    else
-      return;
-
-    if(this.right)
-      this.right.find(value);
-    else
-      return;
-
-    return null;
+    if(leftElement === null){
+      if(this.value === value)
+        return this.value;
+      else if(rightElement !== null)
+        return this.right.find(value);
+    }else {
+      return this.left.find(value);
+    }
+    // return null;
   };
+
+ //  this.find = function(value){  //in-order traversal (left-root-right)
+ //   if(this.left){
+ //     console.log(this, `from this.left`);
+ //     return this.left.find(value);
+ //   }
+ //
+ //   if(this.value === value)
+ //   return this.value;
+ //
+ //   else if(this.right){
+ //     console.log(this, `from this.right`);
+ //     return this.right.find(value);
+ //   }
+ // };
+
+    // if(this.left){
+    //   console.log(`There is a left node`);
+    //   return this.left.find(value);
+    // }else{
+    //   console.log(`There is no left node`);
+    //   if(this.value === value)
+    //     return this;
+    // }
+    //
+    // if(this.right){
+    //   console.log(`There is a right node`);
+    //   return this.right.find(value);
+    // }else{
+    //   if(this.value === value)
+    //     return;
+    // }
+
+
   // this.toString = function(str){  // pre-order traversal (root-left-right)
   //   if(!this)
   //     return;
