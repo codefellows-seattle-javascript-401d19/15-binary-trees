ccc158c48b2e217ea3b95e840a8a16e87d77f956
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-16 09:11:26 -0800

    setting up function signaturues and binary tree constructor function

--- lab-shannon/lib/binaryTrees.js
+++ lab-shannon/lib/binaryTrees.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function BinaryTree(value){
+  this.value = value;
+
+  this.find = function(value){
+
+  };
+  this.toString = function(str){
+
+  };
+  this.toArray = function(array){
+
+  };
+}
