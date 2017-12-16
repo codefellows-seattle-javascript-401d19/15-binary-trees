ffd2a6c171a6a6ec6f267d7c766cbb0dc0a473e4
David Lindahl <davidalindahl@gmail.com>
2017-12-15 17:04:24 -0800

    filling out the prototypes with some code thats not right

--- lab-david/index.js
+++ lab-david/index.js
@@ -13,19 +13,43 @@ const binaryTree = function(value, left, right) {
     this.y = y;
 
   // iterate over all child nodes using an in-order traversal and return first node with the value, return null if not found
-  binaryTree.prototype.find = function(value) {
-    this.value = value;
+  binaryTree.prototype.find = function(root) {
+
+    if(root !== null)
+      return root.value;
+
+    find(root.left);
+    find(root.value);
+    find(root.right);
+
+    return null;
 
   };
 
   // iterate over all nodes using pre-order traversal and concatenate their values separated by newlines into a string
-  binaryTree.prototype.toString = function(str) {
-  
-  
+  binaryTree.prototype.toString = function(root) {
+    let string = '';
+
+    if(root === null)
+      return;
+
+    string = string `+ ${root.value}`;
+    toString(root.left);
+    toString(root.right);
+    return string;
+
   };
 
   // should use post-order traversal push all tree elements into an array
   binaryTree.prototype.toArray = function(array) {
+
+    newArray = [];
+
+    toArray(root.left);
+    toArray(root.right);
+    toArray(root.value);
+
+    newArray.push(value);
 
 
   };  
@@ -37,6 +61,8 @@ let two = new binaryTree(2);
 let three = new binaryTree(3);
 let four = new binaryTree(4);
 let five = new binaryTree(5);
+let six = new binaryTree(6);
+
 
 // setting up binary tree
 one.left = two;
@@ -46,4 +72,3 @@ two.left = four;
 two.right = five;
 
 three.right = six;
-
