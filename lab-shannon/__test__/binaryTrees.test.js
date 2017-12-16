9255caefe4e588b0a2ce60237ecf12d45d252090
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2017-12-16 10:28:23 -0800

    can't figure out how to properly check if I'm calling the function on a proper value in recursive calls

--- lab-shannon/__test__/binaryTrees.test.js
+++ lab-shannon/__test__/binaryTrees.test.js
@@ -15,10 +15,24 @@ describe(`binaryTree.js`, () => {
   three.left = ten;
   three.right = eight;
 
-  test(`The .find method should return the node with the specified value if there are no errors`, () => {
-    expect().toEqual();
+  describe(`Find method`, () => {
+    test(`The .find method should return the node with the specified value if there are no errors`, () => {
+      expect(one.find(7)).toEqual(7);
+    });
+    test(`The .find should return null if no node was found with the specified value`, () => {
+      expect(one.find(22)).toBeNull();
+    });
   });
-  test.only(`The .find should return null if no node was found with the specified value`, () => {
-    expect(one.find(22)).toBeNull();
-  })
+
+  // describe(`toString method`, () => {
+  //   test(`The toString method should return a string of all the elements in a pre-order traversal order`, () => {
+  //     expect(one.toString('')).toEqual('1\n0\n7\n3\n10\n8');
+  //   });
+  // });
+
+  // describe(`toArray method`, () => {
+  //   test(`The toArray method should return an array with all elements in a post-order traversal order`, () => {
+  //     expect(one.toArray([])).toEqual([7,0,1,10,8,3]);
+  //   });
+  // });
 });
--- lab-shannon/lib/binaryTrees.js
+++ lab-shannon/lib/binaryTrees.js
@@ -5,15 +5,31 @@ function BinaryTree(value){
   this.left = null;
   this.right = null;
 
-  this.find = function(value){
+  this.find = function(value){  //in-order traversal (left-root-right)
+    this.left.find(value);
 
-  };
-  this.toString = function(str){
+    if(this.value === value)
+      return this.value;
 
+    this.right.find(value);
   };
-  this.toArray = function(array){
-
-  };
+  // this.toString = function(str){  // pre-order traversal (root-left-right)
+  //   if(!this)
+  //     return;
+  //
+  //   str += this.value + '\n';
+  //   this.left.toString(str);
+  //   this.right.toString(str);
+  //   return str;
+  // };
+  // this.toArray = function(array){ // post-order traversal (left-right-root)
+  //   if(!this)
+  //     return;
+  //
+  //   this.left.toArray(array);
+  //   this.right.toArray(array);
+  //   array.push(this);
+  // };
 }
 
 module.exports = BinaryTree;
