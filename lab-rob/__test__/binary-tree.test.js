412a38ecfb7fc7707bcc31028c03ce42ae1fd7b6
Robert Reed <robert.mc.reed@gmail.com>
2017-12-17 13:09:31 -0800

    toString method working and passing tests

--- lab-rob/__test__/binary-tree.test.js
+++ lab-rob/__test__/binary-tree.test.js
@@ -1,6 +1,23 @@
 'use strict';
 
 const BinaryTree = require('../lib/binary-tree');
+let one, two, three, four, five, six, duplicateThree;
+const setup = () => {
+  one = new BinaryTree(1);
+  two = new BinaryTree(2);
+  three = new BinaryTree(3);
+  four = new BinaryTree(4);
+  five = new BinaryTree(5);
+  six = new BinaryTree(6);
+  duplicateThree = new BinaryTree(3);
+
+  one.left = two;
+  one.right = three;
+  three.left = four;
+  three.right = five;
+  four.left = six;
+  five.right = duplicateThree;
+};
 
 describe('binary-tree.js', () => {
   describe('BinaryTree constructor', () => {
@@ -13,7 +30,64 @@ describe('binary-tree.js', () => {
     });
   });
 
+  describe('BinaryTree methods', () => {
+    beforeEach(setup);
 
+    describe('find(value)', () => {
+      test('find should return the node containing a unique value.', () => {
+        expect(one.find(4)).toEqual(four);
+        expect(four.find(6)).toEqual(six);
+      });
+
+      test('find should return the calling node if the calling node has the desired value', () => {
+        expect(one.find(1)).toEqual(one);
+        expect(six.find(6)).toEqual(six);
+      });
+
+      test('find should return the first node with the desired value, even if there is a duplicate down the tree.', () => {
+        expect(one.find(3)).toEqual(three);
+        expect(three.find(3)).toEqual(three);
+        expect(five.find(3)).toEqual(duplicateThree);
+      });
+
+      test('find should return null if no node has the requested value', () => {
+        expect(one.find('nope')).toBeNull();
+        expect(three.find('nope')).toBeNull();
+      });
+
+
+    });
+
+    describe.only('toString(str)', () => {
+      test('toString should return a concatenated string of all values using a pre-order traversal if an empty string is provided', () => {
+        expect(one.toString('')).toEqual('1\n2\n3\n4\n6\n5\n3');
+        expect(four.toString('')).toEqual('4\n6');
+        expect(six.toString('')).toEqual('6');
+      });
+
+      test('toString should provide an empty string to start if no string is provided.', () => {
+        expect(one.toString()).toEqual('1\n2\n3\n4\n6\n5\n3');
+        expect(four.toString()).toEqual('4\n6');
+        expect(six.toString()).toEqual('6');
+      });
+
+      test('toString should concatenate to the end of the provided string if a nonempty string is provided.', () => {
+        expect(one.toString('hey')).toEqual('hey\n1\n2\n3\n4\n6\n5\n3');
+        expect(four.toString('there')).toEqual('there\n4\n6');
+        expect(six.toString('bob')).toEqual('bob\n6');
+      });
+
+      test('toString should throw an error if a non-string is provided as argument', () => {
+        expect(() => {
+          one.toString(9);
+        }).toThrow();
+      });
+
+
+    });
+
+    
+  });
 
 
 });
--- lab-rob/lib/binary-tree.js
+++ lab-rob/lib/binary-tree.js
@@ -6,4 +6,59 @@ const BinaryTree = function(value) {
   this.right = null;
 };
 
+BinaryTree.prototype.find = function(value) {
+  if(this.left) {
+    this.left.find(value);
+  }
+
+  if(this.value === value) {
+    console.log('found', this.value);
+    return this;
+  } else {
+    console.log('visited', this.value);
+  }
+
+  if(this.right) {
+    this.right.find(value);
+  }
+};
+
+BinaryTree.prototype.toString = function(str = '') {
+  if(typeof str !== 'string')
+    throw new TypeError('argument <str> must be of type string.');
+
+  if(str === '')
+    str += this.value;
+  else
+    str += '\n' + this.value;
+
+  if(this.left)
+    str = this.left.toString(str);
+  
+  if(this.right)
+    str = this.right.toString(str);
+
+  return str;
+};
+
 module.exports = BinaryTree;
+
+
+// let one, two, three, four, five, six, duplicateThree;
+
+// one = new BinaryTree(1);
+// two = new BinaryTree(2);
+// three = new BinaryTree(3);
+// four = new BinaryTree(4);
+// five = new BinaryTree(5);
+// six = new BinaryTree(6);
+// duplicateThree = new BinaryTree(3);
+
+// one.left = two;
+// one.right = three;
+// three.left = four;
+// three.right = five;
+// four.left = six;
+// five.right = duplicateThree;
+
+// console.log(one.toString(''));
