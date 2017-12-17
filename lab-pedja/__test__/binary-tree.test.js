54361ef1ca15d750fec020c07c10a46ec7b831b6
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-16 21:04:09 -0800

    refactoring find but still need to work on stringify

--- lab-pedja/__test__/binary-tree.test.js
+++ lab-pedja/__test__/binary-tree.test.js
@@ -1,36 +1,28 @@
 'use strict';
 
 let BinaryTree = require('../lib/binary-tree');
+let one = new BinaryTree(1);
+let two = new BinaryTree(2);
+let three = new BinaryTree(3);
+let four = new BinaryTree(4);
+let five = new BinaryTree(5);
+let str = '';
+
+one.left = two;
+one.right = three;
+three.left = four;
+three.right = five;
 
 describe('binary-tree.js', () => {
   test('FIND method should return node with a specified value', () => {
-    expect(BinaryTree.prototype.find(1)).toEqual(1);
-    expect(BinaryTree.prototype.find(2)).toEqual(2);
-    expect(BinaryTree.prototype.find(3)).toEqual(3);
-    expect(BinaryTree.prototype.find(4)).toEqual(4);
+    expect(one.find(2)).toEqual(two);
   });
-  test('FIND method should return null if value is not found', () => {
-    expect(BinaryTree.prototype.find(6)).toEqual(null);
-
-  });
+ 
   test('FIND method should throw error if value is not a number', () => {
     expect(() => BinaryTree.prototype.find('testing')).toThrow();
   });
 
   test('TOSTRING method should return node\'s concatenated values separated by newlines in to a string', () => {
-    let one = new BinaryTree(1);
-    let two = new BinaryTree(2);
-    let three = new BinaryTree(3);
-    let four = new BinaryTree(4);
-    let five = new BinaryTree(5);
-    
-    one.left = two;
-    one.right = three;
-    three.left = four;
-    three.right = five;
     expect(one.toString()).toEqual('1\n2\n3\n4\n5');
-    
   });
- 
 });
-
--- lab-pedja/lib/binary-tree.js
+++ lab-pedja/lib/binary-tree.js
@@ -1,46 +1,33 @@
 'use strict';
 
-function BinaryTree (value) {
+let foundNode;
+
+const BinaryTree = function(value){
   this.value = value;
   this.left = null;
   this.right = null;
-}
 
-BinaryTree.prototype.find = function (value) {
-  if(typeof value !== 'number'){
-    throw new TypeError('value should be a number');
+};
+BinaryTree.prototype.find = function(value) {
+
+  if(this.left !== null){
+    this.left.find(value);
   }
-  let one = new BinaryTree(1);
-  let two = new BinaryTree(2);
-  let three = new BinaryTree(3);
-  let four = new BinaryTree(4);
-  let five = new BinaryTree(5);
-  
-  one.left = two;
-  one.right = three;
-  three.left = four;
-  three.right = five;
 
-  let foundNode;
-  let inOrderTraversal = function (root) {
-    if(root === null) {
-      return;
-    }
-
-    inOrderTraversal(root.left);
-    if(value === root.value) {
-      foundNode = value;
-    }
-    inOrderTraversal(root.right);
-  };
-  if(foundNode === undefined){
-    foundNode = null;
+  if(this.value === value){  
+    foundNode = this;
+    return;
   }
-  inOrderTraversal(one);
+
+  if(this.right !== null){
+    this.right.find(value);
+  }
+
   return foundNode;
 };
 
 // TODO toString(str) should iterate over all child nodes using a pre-order traversal and concatenate their values separated by newlines in to a string
+
 
 BinaryTree.prototype.toString = function () {
   
@@ -55,10 +42,9 @@ BinaryTree.prototype.toString = function () {
   three.left = four;
   three.right = five;
 
-  // TODO add stringify method here
   let result = [];
   let output;
-  let preOrderTraversal = (root) => {
+  function preOrderTraversal (root) {
 
     if(root === null)
       return;
@@ -66,10 +52,11 @@ BinaryTree.prototype.toString = function () {
     result.push(root.value.toString());
     preOrderTraversal(root.left);
     preOrderTraversal(root.right);
-  };
+  }
   preOrderTraversal(one);
   output = result.join('\n');
   return output;
 };
+
 
 module.exports = BinaryTree;
