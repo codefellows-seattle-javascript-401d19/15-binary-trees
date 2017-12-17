1227e500703a6f43689e7e1204f3051744b2f6a1
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-12-17 02:28:04 -0800

    test written for preordertostring

--- lab-nicholasc/__test__/binary-tree.test.js
+++ lab-nicholasc/__test__/binary-tree.test.js
@@ -10,7 +10,7 @@ describe('/lib/binary-tree', () => {
       mockTree.appendToHook(9, 10);
       expect(mockTree.left.value).toEqual(9);
     });
-    test.only('inOrderFind', () => {
+    test('inOrderFind', () => {
       let inOrderFindTree = new BinaryTree(10);
       inOrderFindTree.append(9);
       inOrderFindTree.append(8);
@@ -21,13 +21,23 @@ describe('/lib/binary-tree', () => {
       inOrderFindTree.append(3);
       inOrderFindTree.append(2);
       inOrderFindTree.append(1);
+
       expect(inOrderFindTree.inOrderFind(3).value).toEqual(3);
 
     });
-    // test('appendToHook', () => {
-    //   let mockTree = new BinaryTree(10);
-    //   mockTree.appendToHook(9, 10);
-    //   expect(mockTree.left.value).toEqual(9);
-    // });
+    test.only('preOrderToString', () => {
+      let inOrderFindTree = new BinaryTree(10);
+      inOrderFindTree.append(9);
+      inOrderFindTree.append(8);
+      inOrderFindTree.append(7);
+      inOrderFindTree.append(6);
+      inOrderFindTree.append(5);
+      inOrderFindTree.append(4);
+      inOrderFindTree.append(3);
+      inOrderFindTree.append(2);
+      inOrderFindTree.append(1);
+      console.log(inOrderFindTree.preOrderToString());
+      // expect(inOrderFindTree.preOrderToString()).toEqual();
+    });
   });
 });
