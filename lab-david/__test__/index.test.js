4a0b90ff9b76c31e9a723dde14da94c30d4c44ae
David Lindahl <davidalindahl@gmail.com>
2017-12-18 13:56:38 -0800

    refactoring binary tree constructor and tests to include test tree

--- lab-david/__test__/index.test.js
+++ lab-david/__test__/index.test.js
@@ -2,29 +2,47 @@
 
 'use strict';
 
-const index = require('../lib/index');
+const BinaryTree = require('../lib/index');
+
+const testTree = () => {
+  let one = new BinaryTree(1);
+  let two = new BinaryTree(2);
+  let three = new BinaryTree(3);
+  let four = new BinaryTree(4);
+  let five = new BinaryTree(5);
+  let six = new BinaryTree(6);
+
+  one.left = two;
+  one.right = three;
+
+  two.left = four;
+  two.right = five;
+
+  three.right = six;
+};
 
 describe('binary tree - lib/index.js', () => {
+  beforeEach(testTree);
 
   // test find method 
   test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return the first node value that has a value`, () => {
-    expect(index.binaryTree.find(root).toEqual('four'));
+    expect(testTree.find(root).toEqual('four'));
   });
 
   test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return null if a node with a value is not found`, () => {
-    let testTree = ;
-    expect(index.binaryTree.find(root).toEqual('null'));
+    // let testTree = ;
+    expect(testTree.find(root).toEqual('null'));
   });
 
 
   // test toString method
   test(`toString prototype should iterate over all the child nodes using 'pre-order' traversal and concatenate their values into a string separated by newlines`, () => {
-    expect(index.binaryTree.toString(root).toEqual(`\n1, \n2, \n5, \n4, \n3, \n6`));
+    expect(testTree.toString(root).toEqual(`\n1, \n2, \n5, \n4, \n3, \n6`));
   });
 
 
   // test toArray method
   test(`toArray method should iterate over all the child nodes using 'post-order' traversal and push all the trees elements into an array`, () => {
-    expect(index.binaryTree.find(root).toEqual(`[4, 2, 5, 1, 3, 6]`));
+    expect(testTree.find(root).toEqual(`[4, 2, 5, 1, 3, 6]`));
   });
 });
--- lab-david/lib/index.js
+++ lab-david/lib/index.js
@@ -1,48 +1,48 @@
 'use strict';
 
 // setting up constructor
-const binaryTree = function(value) {
+const BinaryTree = function(value) {
   this.value = value;
   this.left = null;
   this.right = null;
 
   // methods on the constructor
   // iterate over all child nodes using an in-order traversal and return first node with the value, return null if not found
-  binaryTree.prototype.find = function(root) {
+  BinaryTree.prototype.find = function(root) {
 
     if(root !== null)
       return root.value;
 
-    binaryTree.find(root.left);
-    binaryTree.find(root.value);
-    binaryTree.find(root.right);
+    BinaryTree.find(root.left);
+    BinaryTree.find(root.value);
+    BinaryTree.find(root.right);
 
     return null;
 
   };
 
   // iterate over all nodes using pre-order traversal and concatenate their values separated by newlines into a string
-  binaryTree.prototype.toString = function(root) {
+  BinaryTree.prototype.toString = function(root) {
     let string = '';
 
     if(root === null)
       return;
 
     string = string `+ ${root.value}`;
-    binaryTree.toString(root.left);
-    binaryTree.toString(root.right);
+    BinaryTree.toString(root.left);
+    BinaryTree.toString(root.right);
     return string;
 
   };
 
   // should use post-order traversal push all tree elements into an array
-  binaryTree.prototype.toArray = function(root) {
+  BinaryTree.prototype.toArray = function(root) {
 
     let newArray = [];
 
-    binaryTree.toArray(root.left);
-    binaryTree.toArray(root.right);
-    binaryTree.toArray(root.value);
+    BinaryTree.toArray(root.left);
+    BinaryTree.toArray(root.right);
+    BinaryTree.toArray(root.value);
 
     newArray.push(value);
 
@@ -52,12 +52,12 @@ const binaryTree = function(value) {
 };
 
 // this is all the nodes 
-let one = new binaryTree(1);
-let two = new binaryTree(2);
-let three = new binaryTree(3);
-let four = new binaryTree(4);
-let five = new binaryTree(5);
-let six = new binaryTree(6);
+let one = new BinaryTree(1);
+let two = new BinaryTree(2);
+let three = new BinaryTree(3);
+let four = new BinaryTree(4);
+let five = new BinaryTree(5);
+let six = new BinaryTree(6);
 
 
 // setting up binary tree
@@ -69,4 +69,4 @@ two.right = five;
 
 three.right = six;
 
-const index = module.exports = {};
+module.exports = BinaryTree;
