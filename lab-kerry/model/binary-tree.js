e931e35aa6c9209a3cd4a3c0402a2d883b9c7b87
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-12-17 19:50:12 -0800

    toString and toArray methods done, tests passing

--- lab-kerry/model/binary-tree.js
+++ lab-kerry/model/binary-tree.js
@@ -1,22 +1,62 @@
 'use strict';
 
-class BinaryTree {
-  //new LinkedList()...
-  constructor(value) {
+const BinaryTree = function(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
 
-//   find(value) {
-//     if (this.next === null && this.value !== value)
-//       return null;
-//     if (this.value === value)
-//       return this;
-//     else
-//       return this.next.find(value);
-//   }
+  // BinaryTree.prototype.appendLeft(root, value) {
 
-// }
+  // }
 
-module.exports = BinaryTree;
+  // BinaryTree.prototype.appendRight(root, value) {
+
+  // }
+
+BinaryTree.prototype.find = function(value) {
+  let foundIt = null;
+
+  if(this.left) {
+    foundIt = this.left.find(value);
+  }
+  if (this.value === value && !foundIt) {
+    foundIt = this;
+  }
+  if (this.right && !foundIt) {
+    foundIt = this.right.find(value);
+  }
+
+  return foundIt;
+};
+
+BinaryTree.prototype.concatToString = function (str = '') {
+  if (str === '') {
+    str += this.value;
+  } else {
+    str += '\n' + this.value;
+  }
+  if (this.left) {
+    str = this.left.concatToString(str);
+  }
+  if (this.right) {
+    str = this.right.concatToString(str);
+  }
+  return str;
+};
+
+BinaryTree.prototype.pushToArray = function (arr = []) {
+  
+  if (this.left !=null) {
+    arr = this.left.pushToArray(arr);
+  }
+  if (this.right !=null) {
+    arr = this.right.pushToArray(arr);
+  }
+  
+  arr.push(this.value);
+
+  return arr;
+};
+
+    module.exports = BinaryTree;
