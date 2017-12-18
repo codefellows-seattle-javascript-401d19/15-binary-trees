2bbc92a0b982c00ac8e8e3edb2b8565b028ce510
David Lindahl <davidalindahl@gmail.com>
2017-12-17 16:20:15 -0800

    updates for test

--- lab-david/__test__/index.test.js
+++ lab-david/__test__/index.test.js
@@ -4,24 +4,27 @@
 
 const index = require('../lib/index');
 
-describe('binary tree - index.js', () => {
-  test(`find method should iterate over all the child nodes using 'in-order' traversal and return the first node value`, () => {
-    expect(index.binaryTree.find(root).toEqual('two'));
-  });
+describe('binary tree - lib/index.js', () => {
 
   // test find method 
-  test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return the first node value`, () => {
-    expect(index.binaryTree.find(root).toEqual('two'));
+  test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return the first node value that has a value`, () => {
+    expect(index.binaryTree.find(root).toEqual('four'));
   });
+
+  test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return null if a node with a value is not found`, () => {
+    let testTree = ;
+    expect(index.binaryTree.find(root).toEqual('null'));
+  });
+
 
   // test toString method
   test(`toString prototype should iterate over all the child nodes using 'pre-order' traversal and concatenate their values into a string separated by newlines`, () => {
-    expect(index.binaryTree.toString(root).toEqual(`1\n2\n3\n4\n5\n6`));
+    expect(index.binaryTree.toString(root).toEqual(`\n1, \n2, \n5, \n4, \n3, \n6`));
   });
 
 
   // test toArray method
   test(`toArray method should iterate over all the child nodes using 'post-order' traversal and push all the trees elements into an array`, () => {
-    expect(index.binaryTree.find(root).toEqual(`[]`));
+    expect(index.binaryTree.find(root).toEqual(`[4, 2, 5, 1, 3, 6]`));
   });
 });
