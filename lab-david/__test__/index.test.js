67ea6bca1599ee8f460e508193171c4175dcd039
David Lindahl <davidalindahl@gmail.com>
2017-12-18 16:11:33 -0800

    wrote todos for tests

--- lab-david/__test__/index.test.js
+++ lab-david/__test__/index.test.js
@@ -1,5 +1,3 @@
-// tests : at least 3 unit tests (2 normal case and one edge case) for every implemented method
-
 'use strict';
 
 const BinaryTree = require('../lib/index');
@@ -26,7 +24,7 @@ describe('binary tree - lib/index.js', () => {
 
   // test find method 
   test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return the first node value that has a value`, () => {
-    expect(testTree.find(4).toEqual('four'));
+    expect(BinaryTree.find(4).toEqual('four'));
   });
 
   test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return null if a node with a value is not found`, () => {
@@ -34,11 +32,19 @@ describe('binary tree - lib/index.js', () => {
     expect(testTree.find(100).toEqual('null'));
   });
 
+  // TODO : write another test for find proto - edge case
+
+
 
   // test toString method
   test(`toString prototype should iterate over all the child nodes using 'pre-order' traversal and concatenate their values into a string separated by newlines`, () => {
     expect(testTree.toString(1).toEqual(`\n1, \n2, \n5, \n4, \n3, \n6`));
   });
+
+  // TODO : write another test for toString - normal case
+
+  // TODO : write another test for toString - edge case
+
 
 
   // test toArray method
@@ -46,3 +52,8 @@ describe('binary tree - lib/index.js', () => {
     expect(testTree.find(1).toEqual(`[4, 2, 5, 1, 3, 6]`));
   });
 });
+
+  // TODO : write another test for toArray - normal case
+
+    // TODO : write another test for toArray - edge case
+
