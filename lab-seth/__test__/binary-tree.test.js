6c0dd40a9e28f0281299cd0c73b8c49848b9b935
SethDonohue <donohue.seth@gmail.com>
2017-12-18 23:46:01 -0800

    all tests GREEN for all methods

--- lab-seth/__test__/binary-tree.test.js
+++ lab-seth/__test__/binary-tree.test.js
@@ -23,20 +23,20 @@ const treeBuilder = () => {
 describe('Binary Tree JS Functions', () => {
   // afterEach(() => binaryTree.remove({}));
   describe('Find() prototype', () => {
-    test('This test should pass when a normal case value is searched for', () => {
+    test('This test should return a node that has the value searched for', () => {
       let tree = treeBuilder();
       expect(tree.find(6)).toEqual(tree.right.left.left);
       expect(tree.find(7)).toEqual(tree.right.left.right);
       expect(tree.find(1)).toEqual(tree);
     });
     
-    test(' This test should PASS if a non-number is searched for', () => {
+    test(' This test should return null if a non-number is searched for', () => {
       let tree = treeBuilder();
       expect(tree.find(null)).toBeNull();
       expect(tree.find('string')).toBeNull();
     });
 
-    test(' This test should PASS if the value searched for does not exist in the tree', () => {
+    test(' This test should return null if the value searched for does not exist in the tree', () => {
       let tree = treeBuilder();
       expect(tree.find(10)).toBeNull();
       expect(tree.find(-10)).toBeNull();
@@ -44,16 +44,36 @@ describe('Binary Tree JS Functions', () => {
   });
 
   describe('ToString() prototype', () => {
-    test('This test should pass and return a string when called on a binary tree', () => {
+    test('This test should return a string when called on the first node of a binary tree', () => {
       let tree = treeBuilder();
       expect(tree.toString()).toEqual('1\n2\n3\n4\n6\n7\n5');
+    });
+
+    test('This test should return a concatinated string when called on the first node of a binary tree and when passed a starting string', () => {
+      let tree = treeBuilder();
+      expect(tree.toString('stuff')).toEqual('stuff\n1\n2\n3\n4\n6\n7\n5');
+    });
+
+    test('This test should return a string when called on any node but the first node of a binary tree', () => {
+      let tree = treeBuilder();
+      expect(tree.left.toString()).toEqual('2');
     });
   });
 
   describe('ToArray() prototype', () => {
-    test('This test should pass and return an array when called on a binary tree', () => {
+    test('This test should return an array of all the values of the nodes if this method is called on the first node of a binary tree and no array is passed as an argument', () =>{
       let tree = treeBuilder();
-      expect(tree.toArray()).toEqual([2,6,7,4,5,3,1]);
+      expect(tree.toArray()).toEqual([2, 6, 7, 4, 5, 3, 1]);
+    });
+
+    test('This test should return an array when called on a binary tree that has the values of the nodes pushed onto the original array passed in as an argument', () => {
+      let tree = treeBuilder();
+      expect(tree.toArray([123])).toEqual([123,2,6,7,4,5,3,1]);
+    });
+
+    test('This test should return an array when called on any node but the first node of a binary tree', () => {
+      let tree = treeBuilder();
+      expect(tree.left.toArray()).toEqual([2]);
     });
   });
 });
