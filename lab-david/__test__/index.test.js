bf281ee5b38e4d73cdcfca28cb1aa59fe7f7a6f8
David Lindahl <davidalindahl@gmail.com>
2017-12-19 16:46:14 -0800

    all tests passing

--- lab-david/__test__/index.test.js
+++ lab-david/__test__/index.test.js
@@ -23,34 +23,32 @@ describe('binary tree - lib/index.js', () => {
   // test find method 
   test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return the first node value that has a value`, () => {
     expect(one.find(1)).toEqual(one);
+    expect(one.find(3)).toEqual(three);
+
   });
 
-  test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return null if a node with a value is not found`, () => {
+  test(`find prototype should iterate over all the child nodes using 'in-order' traversal and return null if a node with a value is not found | edge case`, () => {
     expect(one.find(100)).toEqual(null);
   });
-
-  // TODO : write another test for find proto - edge case
-
-
 
   // test toString method
   test(`toString prototype should iterate over all the child nodes using 'pre-order' traversal and concatenate their values into a string separated by newlines`, () => {
     expect(one.toString()).toEqual(`1\n2\n4\n5\n3\n6`);
+    expect(two.toString()).toEqual(`2\n4\n5`);
   });
 
-  // TODO : write another test for toString - normal case
-
-  // TODO : write another test for toString - edge case
-
-
+  test(`toString prototype should iterate over all the child nodes using 'pre-order' traversal and return null if not given a thing | edge case`, () => {
+    expect(four.toString()).toEqual(`4`);
+  });
 
   // test toArray method
   test(`toArray method should iterate over all the child nodes using 'post-order' traversal and push all the trees elements into an array`, () => {
     expect(one.toArray([])).toEqual([4, 5, 2, 6, 3, 1]);
+    expect(two.toArray([])).toEqual([4, 5, 2]);
   });
+
+  test(`toArray method should iterate over all the child nodes using 'post-order' traversal and push all the trees elements into an array | edge case`, () => {
+    expect(four.toArray([])).toEqual([4]);
+  });
+
 });
-
-  // TODO : write another test for toArray - normal case
-
-    // TODO : write another test for toArray - edge case
-
